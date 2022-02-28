6c82cfeb0c960a974558468e8d10b9a6bb8f409a
Tasheron <alex-2004.ru@yandex.ru>
2022-02-28 21:16:51 +0300

    project done

---
+++ main.py
@@ -0,0 +1,20 @@
+from email.policy import default
+import calc
+import view
+
+dictionary = \
+    {
+        "+": calc.sum,
+        "-": calc.dif,
+        "*": calc.mult,
+        "/": calc.div,
+        "**": calc.pow
+    }
+
+oper = input("Введите оператор: ")
+
+rez = round(calc.init(dictionary[oper], view.a, view.b), 10)
+if rez - int(rez) == 0:
+    rez = int(rez)
+
+print(f"Результат операции равен {rez}")
